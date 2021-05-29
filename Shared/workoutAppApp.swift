06094eddb1f1748451c32448dbbb1c17b2cfb6bb
Abdullah Ridwan <abdullahridwan73@gmail.com>
2021-05-29 15:59:39 -0400

    Initial Commit

---
+++ Shared/workoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutAppApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct workoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
